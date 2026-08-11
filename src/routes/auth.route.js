516571db29df5b4f87ada0205a908a9da81475f6
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2026-08-11 12:34:33 +0330

    [REFACTOR]

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -7,7 +7,7 @@ import {
   signup,
   updateProfile,
 } from "../controllers/auth.controller.js";
-import { protectRoute } from "../models/auth.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
