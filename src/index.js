3ae4bc43df7850344c59da8112d8e08c8b13d6ca
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2026-08-11 13:43:44 +0330

    [ADD] message api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 
 import { connectDB } from "./lib/db.lib.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT || 5000;
 
